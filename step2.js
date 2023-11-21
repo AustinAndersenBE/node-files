0dbcd8326b8253c26ebbf90174aac262da70b0e0
Austin <senhua@DESKTOP-5CE9SR5>
2023-11-20 20:35:09 -0800

    initial commit

---
+++ step2.js
@@ -0,0 +1,31 @@
+const fs = require('fs');
+const axios = require('axios');
+
+function cat(path) {
+    fs.readFile(path, 'utf8', (err, data) => {
+        if (err) {
+            console.error(`Error reading ${path}:\n ${err}`);
+            process.exit(1);
+        }
+        console.log(data);
+    });
+}
+
+
+async function webCat(url) {
+    try {
+        let resp = await axios.get(url);
+        console.log(resp.data);
+    } catch (err) {
+        console.error(`Error fetching ${url}:\n ${err}`);
+        process.exit(1);
+    }
+}
+
+const pathOrUrl = process.argv[2];
+
+if (pathOrUrl.startsWith('http://') || pathOrUrl.startsWith('https://')) {
+    webCat(pathOrUrl);
+} else {
+    cat(pathOrUrl);
+}
